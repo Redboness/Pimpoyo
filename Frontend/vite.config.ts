f28b60879c11b6f85340397296a1c5dc0e0f4f8d
Redboness <lalaboom400@gmail.com>
2025-06-03 17:58:21 +0200

    Añadir sajl.cc a los domminios permitidos

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -2,18 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
+// Básicamente decimos en que puerto se hostea la aplicación y en que "direcciones" se permite acceder a ella
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
     port: 5173,
-    allowedHosts: ["cosmic-thankful-humpback.ngrok-free.app"], // CAMBIAR UNICAMENTE A DOMINIO!
+    allowedHosts: ["cosmic-thankful-humpback.ngrok-free.app", "sajl.cc"],
     proxy: {
       // Requests starting with /api will be forwarded to your backend
       '/api': {
-        target: 'http://localhost:8000', // Your FastAPI server address (localhost is fine here)
-        changeOrigin: true, // Recommended for most setups
-        rewrite: (path) => path.replace(/^\/api/, ''), // Remove /api prefix before sending to backend
+        target: 'http://localhost:8000', // Donde sea que esté corriendo el backend, en este caso en local en el puerto 8000-
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''), // Quitar /api del path, se añade para evitar conflictos.
       },
     },
   },
